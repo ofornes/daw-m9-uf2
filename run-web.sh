8225908d3e8ac3197bb2948a64e23e0679b6bb1f
Octavi Fornés <octavi@fornes.cat>
2022-10-16 22:49:07 +0200

    Esmena de l'script Esmena dels paths de l'arxiu navegacio.html

--- run-web.sh
+++ run-web.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -p 80:80 --name nginx -v "$(pwd -P)/resolucio-html":/usr/share/nginx/html:ro -d nginx
+docker run --rm -p 80:80 --name nginx -v "$(pwd -P)/CAS2/resolucio-html":/usr/share/nginx/html:ro -d nginx
