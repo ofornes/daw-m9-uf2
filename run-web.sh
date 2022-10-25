f5a32408a9821f31647ab7f68579890a9ebe41e6
Octavi Fornés <octavi@fornes.cat>
2022-10-25 23:08:16 +0200

    Afegit text de la pàgina de serveis

--- run-web.sh
+++ run-web.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -p 80:80 --name nginx -v "$(pwd -P)/pages":/usr/share/nginx/html:ro -d nginx
+docker run --rm -p 80:80 --name nginx -v "$(pwd -P)/docs":/usr/share/nginx/html:ro -d nginx
